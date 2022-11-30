feb5f6f2ef1b97adf56f0c3cafc72a9511de9b87
Samuel vergniol <samuelvgnl@gmail.com>
2022-11-30 16:35:48 +0100

    Update hardhat.config.js

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+// https://eth-goerli.g.alchemy.com/v2/J3ye2NpLgBTDX_aAM0uw7Qm4Gc2-JD6P
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomiclabs/hardhat-waffle");
+
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.0",
+  networks: {
+    goerli: {
+      url: "https://eth-goerli.g.alchemy.com/v2/J3ye2NpLgBTDX_aAM0uw7Qm4Gc2-JD6P",
+      accounts: [
+        "66f4a3aca0c44cb6db08d715ee39dcff0a37847e6afbd3cb0bcdf14b99b57497",
+      ],
+    },
+  },
 };
